6af6f729294ccc0ac0fba90245227026e9020388
DarshitSorathiya <darshitsorthiya@gmail.com>
2025-07-12 10:05:06 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = 3000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERROR : ", err);
+      throw err;
+    });
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
